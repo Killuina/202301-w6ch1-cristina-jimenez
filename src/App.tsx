cdb9cad80bc8e98d4fb0c5701afb075f6b21e884
Cristina <cristinajmartin98@gmail.com>
2023-02-14 20:41:53 +0100

    Add style reset and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import TaskCard from "./components/TaskCard/taskCard";
+
 const App = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <header>
+        <h1>ToDo App 📝</h1>
+      </header>
+      <main>
+        <TaskCard task={{ id: 1, name: "Hacer la colada", isDone: false }} />
+      </main>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface TaskStructure {
+  id: number;
+  name: string;
+  isDone: boolean;
+}
+
+export type Tasks = TaskStructure[];
